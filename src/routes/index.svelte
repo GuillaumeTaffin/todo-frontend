c42a598a5a1a406cfac04c72f1228a360802abc8
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2022-05-10 23:43:04 -0400

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,32 @@
+<script>
+  import {browser} from "$app/env";
+
+  let counter;
+  let increment;
+  let decrement;
+
+  if(browser) {
+    const socket = new WebSocket("ws://ktor-todo-backend-stream.herokuapp.com/")
+
+    socket.onopen = function (_) {
+      console.log("connected to server")
+    };
+
+    socket.onmessage = (event) => {
+        console.log(event.data)
+        counter = JSON.parse(event.data).count
+    }
+
+    increment = () => socket.send("increment")
+    decrement = () => socket.send("decrement")
+  }
+
+</script>
+
+<h1>Counter : {counter ?? "Loading..."}</h1>
+<button on:click={increment}>
+    INCREMENT
+</button>
+<button on:click={decrement}>
+    DECREMENT
+</button>
